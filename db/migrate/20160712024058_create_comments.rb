9424c3acff6f6c88e64eca46cc9bf76248bc0917
parkhaeun <syhe617@gmail.com>
2016-07-13 10:28:34 +0900

    final

---
+++ db/migrate/20160712024058_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :commenter
+      t.string :string
+      t.text :body
+
+     t.references :article, index: true
+ 
+      t.timestamps
+    end
+  end
+end
